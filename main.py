d56b8ac1872c0571043f4a7b8e33e99f5ea6525e
lOKESH <dnishi213@gmail.com>
2024-10-21 17:17:15 +0530

    added model evaluation pipeline completely

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ from src.cnnClassifier import logger
 from cnnClassifier.pipeline.stage_1_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_2_prepare_base_model import PrepareBaseModelTrainingPipeline
 from cnnClassifier.pipeline.stage_3_model_trainer import ModelTrainingPipeline
+from cnnClassifier.pipeline.stage_4_model_evaluation import EvaluationPipeline
 
 # logger.info(f'Welcome! project starts now.')
 
 STAGE_NAME = "Data Ingestion stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    obj = DataIngestionTrainingPipeline()
-    obj.main()
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
@@ -36,3 +37,15 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+except Exception as e:
+        logger.exception(e)
+        raise e
