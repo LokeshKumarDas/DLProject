8c192662a0b296e91292ebb212019cffaee44bc2
lOKESH <dnishi213@gmail.com>
2024-10-21 14:47:10 +0530

    created base model pipeline

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from src.cnnClassifier import logger
 
 
 from cnnClassifier.pipeline.stage_1_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_2_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 
@@ -18,3 +19,14 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
